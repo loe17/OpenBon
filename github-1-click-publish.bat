8b72143ca60b8212f269411c9d0d5c763af9d8bc
lks17 <lukas.oehler@outlook.com>
2026-08-21 21:37:24 +0200

    OpenBon Release Update

--- github-1-click-publish.bat
+++ github-1-click-publish.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal enabledelayedexpansion
 
-REM Stelle sicher, dass das Arbeitsverzeichnis der Ordner dieser Batch-Datei ist
+REM Arbeitsverzeichnis auf Ordner dieser Batch-Datei festlegen
 cd /d "%~dp0"
 
 title OpenBon - 1-Klick GitHub Veroeffentlichung
@@ -15,22 +15,21 @@ REM 1. Pruefen ob Git installiert ist
 where git >nul 2>nul
 if %ERRORLEVEL% NEQ 0 (
     echo [FEHLER] Git ist auf diesem PC nicht installiert oder nicht im PATH!
-    echo Bitte lade Git fuer Windows herunter:
-    echo https://git-scm.com/download/win
+    echo Bitte lade Git fuer Windows herunter: https://git-scm.com/download/win
     echo.
     pause
     exit /b 1
 )
 
-REM 2. Git Repository initialisieren falls noetig
-if not exist ".git" (
+REM 2. Git Repository initialisieren falls noch nicht vorhanden
+if not exist .git (
     echo [1/4] Initialisiere lokales Git Repository...
     git init
     git branch -M master
 )
 
 REM 3. Remote URL auf https://github.com/loe17/OpenBon.git setzen
-echo [2/4] Konfiguriere GitHub Remote Ziel (https://github.com/loe17/OpenBon.git)...
+echo [2/4] Konfiguriere GitHub Remote Ziel: https://github.com/loe17/OpenBon.git
 git remote remove origin >nul 2>nul
 git remote add origin https://github.com/loe17/OpenBon.git
 
@@ -38,14 +37,9 @@ REM 4. Alle Dateien erfassen und committen
 echo [3/4] Erfasse Dateien und erstelle Commit...
 git add .
 git commit -m "OpenBon Release Update" >nul 2>nul
-if %ERRORLEVEL% EQU 0 (
-    echo   -> Aenderungen wurden erfasst und committet.
-) else (
-    echo   -> Keine neuen lokalen Datei-Aenderungen gefunden (bereits aktuell).
-)
 
 REM 5. Auf GitHub hochladen
-echo [4/4] Lade Daten auf GitHub hoch (git push origin master)...
+echo [4/4] Lade Daten auf GitHub hoch [git push origin master]...
 echo.
 git push -u origin master --force
 
@@ -59,13 +53,13 @@ if %ERRORLEVEL% EQU 0 (
     echo.
     echo ======================================================================
     echo   [HINWEIS ZUR GITHUB-ANMELDUNG]
-    echo   Falls GitHub nach Benutzername/Passwort fragt:
+    echo   Falls GitHub nach Benutzername oder Passwort fragt:
     echo   1. Benutzername: loe17
-    echo   2. Passwort: Dein GitHub Personal Access Token (PAT)
-    echo      (Erstelle ihn unter https://github.com/settings/tokens)
+    echo   2. Passwort: Dein GitHub Personal Access Token [PAT]
+    echo      Erstelle ihn unter: https://github.com/settings/tokens
     echo ======================================================================
 )
 
 echo.
-echo Druecke eine beliebige Taste zum Beenden...
+echo Druecke eine beliebige Taste zum Schliessen...
 pause >nul
